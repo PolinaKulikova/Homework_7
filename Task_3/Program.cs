d5229ba519eaaa0ed33e40654fdf5319a5ebe595
Polina Kulikova <pomidorishna@gmail.com>
2023-01-19 21:34:04 +0300

    Задача 3  готова

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Console.WriteLine("Введите количество строк");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[row, column];
+FillArray(matrix);
+Console.WriteLine("Задан массив:");
+PrintMatrix(matrix);
+
+void PrintMatrix(int[,] matr)              // Печать матрицы
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)                      // Заполнение матрицы рандомными числами [1, 10)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double average = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        average = (average + matrix[i, j]);
+    }
+    average = average / row;
+    
+    Console.Write((Math.Round(average, 1)) + "; ");
+}
